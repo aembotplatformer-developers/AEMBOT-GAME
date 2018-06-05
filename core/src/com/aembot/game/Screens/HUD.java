f2c0d03e0af041c091460b3bb4497015e3ba148a
OwenE20 <owenengbretson@gmail.com>
2018-06-05 11:00:10 -0700

    HUD Changes + added Box2D basics

--- core/src/com/aembot/game/Screens/HUD.java
+++ core/src/com/aembot/game/Screens/HUD.java
@@ -29,14 +29,14 @@ public class HUD {
     stage = new Stage(viewport,HudBatch);
 
     levelL = new com.badlogic.gdx.scenes.scene2d.ui.Label((String.format("%02d",level)), new com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle(new BitmapFont(), Color.WHITE));
-    scoreL = new com.badlogic.gdx.scenes.scene2d.ui.Label((String.format("%03d",score)), new com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle(new BitmapFont(), Color.WHITE));
+    scoreL = new com.badlogic.gdx.scenes.scene2d.ui.Label((String.format("%06d",score)), new com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle(new BitmapFont(), Color.WHITE));
     ammoL = new com.badlogic.gdx.scenes.scene2d.ui.Label((String.format("%03d",ammo)), new com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle(new BitmapFont(), Color.WHITE));
     levelTitle = new com.badlogic.gdx.scenes.scene2d.ui.Label((String.format("Level")), new com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle(new BitmapFont(), Color.WHITE));
     scoreTitle = new com.badlogic.gdx.scenes.scene2d.ui.Label((String.format("Score")), new com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle(new BitmapFont(), Color.WHITE));
     ammoTitle = new com.badlogic.gdx.scenes.scene2d.ui.Label((String.format("Ammo")), new com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle(new BitmapFont(), Color.WHITE));
 
     Table table = new Table();
-    table.setY(150);
+    table.setY(viewport.getScreenY() - 50);
     table.add(levelTitle).pad(10);
     table.add(scoreTitle).pad(10);
     table.add(ammoTitle).pad(10);
@@ -44,6 +44,7 @@ public class HUD {
     table.add(levelL).pad(10);
     table.add(scoreL).pad(10);
     table.add(ammoL).pad(10);
+
 
     stage.addActor(table);
     }
--- core/src/com/aembot/game/Screens/PlayScreen.java
+++ core/src/com/aembot/game/Screens/PlayScreen.java
@@ -7,6 +7,9 @@ import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -17,13 +20,23 @@ public class PlayScreen implements Screen {
     private OrthographicCamera gamecam;
     private Viewport gamePort;
     private HUD hud;
+    private World world;
+    private Box2DDebugRenderer b2dr;
+
 
     public PlayScreen(AembotPlatformer game) {
         this.game = game;
      //   texture = new Texture("badlogic.jpg");
         gamecam = new OrthographicCamera();
+
         gamePort = new StretchViewport(800, 480, gamecam);
+
         hud = new HUD(game.batch,0,0,0,this);
+
+        world = new World(new Vector2(0,9.8f),true);
+
+        b2dr = new Box2DDebugRenderer();
+
     }
 
     @Override
@@ -35,13 +48,18 @@ public class PlayScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        b2dr.render(world,gamecam.combined);
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
-        //game.batch.begin();
-      //  game.batch.draw(texture, 0, 0);
-        //game.batch.end();
 
 
+
+
+    }
+
+    public void update(float dt){
+
+        world.step(dt,8,3);
 
     }
 
