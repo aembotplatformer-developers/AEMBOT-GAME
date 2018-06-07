9d27b46b02185386ce8ea214e57ea0aef1285e2a
Goirick Saha <sahagoirick@gmail.com>
2018-06-07 03:27:56 -0700

    Added corresponding map files and project files

---
+++ core/assets/NES - Wai Wai World JPN - Castle Dracula Background Set rendered.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="NES - Wai Wai World JPN - Castle Dracula Background Set rendered" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="1652" columns="59">
+ <image source="NES - Wai Wai World JPN - Castle Dracula Background Set rendered.png" width="1067" height="514"/>
+</tileset>
---
+++ core/assets/SNES - Kirbys Dream Land 3 - Grass Land Foreground Set 1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="SNES - Kirbys Dream Land 3 - Grass Land Foreground Set 1" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="4864" columns="76">
+ <image source="SNES - Kirbys Dream Land 3 - Grass Land Foreground Set 1.png" width="1380" height="1155"/>
+</tileset>
